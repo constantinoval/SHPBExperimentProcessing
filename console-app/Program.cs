184e1893a12f4c2b4f06976decc8f09623586834
constantinoval <constantinov.al@yandex.ru>
2021-02-07 16:24:41 +0300

    initial commit

---
+++ console-app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using lib;
+using lib2;
+using ScottPlot;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new();
+            Class2 c2 = new();
+            Class3 c3 = new();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
